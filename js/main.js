d75829bb4451bb13d7f19223a53f00841b3ca91d
Ajij Rahaman <ajij@appycodes.com>
2023-08-09 11:40:32 +0530

    2.nd commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+let inputField = document.getElementById("inputField");
+
+let testBtn = document.getElementById("testBtn");
+
+testBtn.addEventListener("click", function(){
+
+    const message = document.getElementById("inputField").value;
+    message.innerHTML = "";
+
+    try {
+        if(message.trim() == "") throw "empty";
+        const numberValue = Number(message);
+        if(isNaN(numberValue)) throw "not a number";
+        if(numberValue < 5) throw "too low";
+        if(numberValue > 10) throw "too high";
+
+    }
+    catch(err){
+        showMessage.innerHTML = "input is " + err;
+    }
+
+});
